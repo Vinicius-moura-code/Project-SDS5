d321ef5b6a04172cb035151c4daf20b567f50ec8
Vinicius Moura <viniciusrebelo487@gmail.com>
2021-11-01 18:48:25 -0300

    Add charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import Datatable from "components/Datatable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 import React from "react";
@@ -8,6 +10,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-primary">Todas vendas</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-primary">Todas vendas</h5>
+            <DonutChart />
+          </div>
+          <div className="py-3">
+            <h2 className="text-primary">Todas Vendas</h2>
+          </div>
+        </div>
         <Datatable />
       </div>
       <Footer />
